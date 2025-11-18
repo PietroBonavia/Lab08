e5ac493bf131239d73c2eeab45a4d83555eed993
PietroBonavia <pietro.bonavia@icloud.com>
2025-11-18 12:10:09 +0100

    lab08

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 from database.impianto_DAO import ImpiantoDAO
+from database.consumo_DAO import ConsumoDAO
 
 '''
     MODELLO:
@@ -26,6 +27,26 @@ class Model:
         :return: lista di tuple --> (nome dell'impianto, media), es. (Impianto A, 123)
         """
         # TODO
+        consumo_medio = list()
+
+        for imp in self._impianti:
+            consumi = ConsumoDAO.get_consumi(imp.id)
+            somma = 0
+            contatore = 0
+
+            for c in consumi:
+                mese_consumo = c.data.month
+
+                if mese_consumo == mese:
+                    somma += c.kwh
+                    contatore += 1
+
+            media = somma / contatore
+            consumo_medio.append((imp.nome, media))
+
+        return consumo_medio
+
+
 
     def get_sequenza_ottima(self, mese:int):
         """
@@ -47,6 +68,38 @@ class Model:
     def __ricorsione(self, sequenza_parziale, giorno, ultimo_impianto, costo_corrente, consumi_settimana):
         """ Implementa la ricorsione """
         # TODO
+        if giorno == 8:
+            if self.__costo_ottimo == -1 or costo_corrente < self.__costo_ottimo:
+               self.__costo_ottimo = costo_corrente
+               self.__sequenza_ottima = sequenza_parziale.copy()
+            return
+
+        for imp in self._impianti:
+            imp_id = imp.id
+
+
+            consumo_giorno = consumi_settimana[imp_id][giorno-1]
+
+            costo_spostamento = 0
+            if ultimo_impianto is not None and ultimo_impianto != imp_id:
+                costo_spostamento = 5
+
+            nuovo_costo = costo_corrente + consumo_giorno + costo_spostamento
+
+            sequenza_parziale.append(imp_id)
+
+            self.__ricorsione(
+                sequenza_parziale,
+                giorno + 1,
+                imp_id,
+                nuovo_costo,
+                consumi_settimana
+            )
+
+            sequenza_parziale.pop()
+
+
+
 
     def __get_consumi_prima_settimana_mese(self, mese: int):
         """
@@ -55,3 +108,22 @@ class Model:
         """
         # TODO
 
+        consumi_settimana = dict()
+
+        for impianto in self._impianti:
+            consumi = ConsumoDAO.get_consumi(impianto.id)
+
+            lista_giorni = []
+
+            for c in consumi:
+                if c.data.month == mese and 1 <= c.data.day <= 7:
+                    lista_giorni.append((c.data.day, c.kwh))
+
+            # ordina per giorno
+            lista_giorni.sort(key=lambda x: x[0])
+
+            kwh_ordinati = [k for (day, k) in lista_giorni]
+
+            consumi_settimana[impianto.id] = kwh_ordinati
+
+        return consumi_settimana
